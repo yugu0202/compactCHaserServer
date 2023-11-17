aa08a50157609e0b73903fb3750b00938e23e659
yugu0202 <49861024+yugu0202@users.noreply.github.com>
2023-11-17 11:48:37 +0900

    Update DumpSystem.py

--- DumpSystem.py
+++ DumpSystem.py
@@ -1,14 +1,14 @@
 class DumpSystem:
-    def __init__(self,dump_flag:bool,log_path:str,map_path:str):
+    def __init__(self,non_dump:bool,log_path:str,map_path:str):
         self.path = log_path
-        self.dump_flag = dump_flag
-        if not self.dump_flag:
+        self.non_dump = non_dump
+        if self.non_dump:
             return
         with open(map_path, mode="r") as f:
             self.map_data = "".join(list(map(lambda x: x[2:],f.readlines())))
 
     def set_name(self,cool_name:str,hot_name:str):
-        if not self.dump_flag:
+        if self.non_dump:
             return
         with open(self.path,mode="w") as f:
             f.write(f"{cool_name},{hot_name}\n")
@@ -16,13 +16,13 @@ class DumpSystem:
             f.write("0,0");
 
     def action(self,map_data:str,cool_pos:str,hot_pos:str,cool_item:int,hot_item:int):
-        if not self.dump_flag:
+        if self.non_dump:
             return
         with open(self.path,mode="a") as f:
             f.write(f"\n{map_data}\n{cool_pos}\n{hot_pos}\n{cool_item},{hot_item}")
 
     def result(self,player:str,result:str,reason:str):
-        if not self.dump_flag:
+        if self.non_dump:
             return
         winner = None
         if result == "lose":
